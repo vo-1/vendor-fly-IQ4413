4e41ddd037350dcf4c07c42f556cdc2bb5b3fb62
dee3000 <dee3000@bk.ru>
2016-06-09 16:58:31 +0300

    first commit

---
+++ AQ5001-vendor.mk
@@ -0,0 +1,12 @@
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/micromax/AQ5001/bin,system/bin)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/micromax/AQ5001/lib,system/lib)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/micromax/AQ5001/etc,system/etc)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/micromax/AQ5001/vendor,system/vendor)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/micromax/AQ5001/xbin,system/xbin)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/micromax/AQ5001/app,system/app)
+
